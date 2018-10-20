d6fc2d012faec946a5c30815c0024b3c5021b374
camposLuis <luis.campos.henrique@hotmail.com>
2018-10-20 16:08:40 -0300

    Alteração projeto

--- src/main/java/com/example/algamoneyapi/repository/LancamentoRepository.java
+++ src/main/java/com/example/algamoneyapi/repository/LancamentoRepository.java
@@ -3,7 +3,7 @@ package com.example.algamoneyapi.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.algamoneyapi.model.Lancamento;
-import com.example.algamoneyapi.repository.lancamento.LancamentoRepositoryQuery;
+import com.example.algamoneyapi.repository.query.LancamentoRepositoryQuery;
 
 public interface LancamentoRepository extends JpaRepository<Lancamento, Long>, LancamentoRepositoryQuery {
 
--- src/main/java/com/example/algamoneyapi/repository/PessoaRepository.java
+++ src/main/java/com/example/algamoneyapi/repository/PessoaRepository.java
@@ -3,7 +3,7 @@ package com.example.algamoneyapi.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.algamoneyapi.model.Pessoa;
-import com.example.algamoneyapi.repository.lancamento.PessoaRepositoryQuery;
+import com.example.algamoneyapi.repository.query.PessoaRepositoryQuery;
 
 public interface PessoaRepository extends JpaRepository<Pessoa, Long>, PessoaRepositoryQuery {
 
